6004f7c81217b8e080e3e131f3ca02c18e53cff4
Iuliusgh <iuliusgh@ucm.es>
2024-05-18 13:44:38 +0200

    eleccion estacion
    subo el script para la eleccion de la estacion con comentarios

---
+++ 1 Preprocesado/eleccion_estacion.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Se ha realizado un estudio de la completitud de los datos de las diferentes estaciones meteorológicas 
+de Madrid para elegir la que tuviera los datos más completos. De esta manera se busca obtener los mejores resultados
+para los modelos predictivos.
+"""
+
+import pandas as pd
+import os
+import numpy as np
+#función para sustituir el valor 88 (indeterminado) de la dirección del viento por nan
+def substitute_value(row):
+    if row['dir'] == 88:
+        return np.nan
+    else:
+        return row['dir']
+#columnas importantes para la predicción
+columnas = ['filename','prec','tmin','tmax','dir','velmedia','racha','sol','total']
+#se crea un dataframe con las columnas importantes
+result = pd.DataFrame(columns=columnas)
+#ruta a la carpeta que contiene todos los ficheros json de las distintas estaciones meteorologicas
+path = '../0 data/meteorologia'
+#Para cada fichero
+for filename in os.listdir(path):
+    #Se abre el archivo
+    with open(os.path.join(path,filename), 'r') as f:
+        #Se cargan los valores en un dataframe
+        df = pd.read_json(f)
+        #Para cada columna importante
+        for col in columnas:
+            #Si no está en el archivo
+            if col not in df.columns:
+                #Se agrega una columna con valores nan
+                df[col]=np.nan
+        #Se seleccionan unicamente las columnas importantes
+        df = df[['prec','tmin','tmax','dir','velmedia','racha', 'sol']]
+        #Se sustituyen los valores 88 de la direccion del viento
+        df['dir'] = df.apply(substitute_value, axis=1)
+        #Se claculan los valores faltantes de cada columna
+        nulls = df.isnull().sum()
+        #Se suma el total de nulos en cada dataframe
+        total = nulls['prec'] + nulls['tmin'] + nulls['tmax'] + nulls['dir'] + nulls['velmedia'] + nulls['racha'] + nulls['sol']
+        #Se crea una nuea fila que contiene todos los datos importantes que se quieren mostrar
+        row = [filename[2:-5],nulls['prec'],nulls['tmin'],nulls['tmax'],nulls['dir'],nulls['velmedia'],nulls['racha'],nulls['sol'],total]
+        #Se añade la fila creada al dataframe resultado
+        result.loc[len(result)] = row
+        #se cierra el archivo
+        f.close()
+#Se agrrupan las filas de la misma estación meteorológica        
+result_ordered = result.groupby('filename').sum()
+#Se ordenan por el total de nulos de menor a mayor
+result_ordered = result_ordered.sort_values(by='total')
+#Se muestra el resultado
+print(result_ordered)
